36a433000d503c5de6c1f295df82d23e6eb66d9e
alaelbenna <elbenna.ala@outlook.com>
2022-02-05 05:20:24 -0800

    best_score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-def multiply_by_2(a_dictionary):
-    d = dict()
-    for i, j in a_dictionary.items():
-        d[i] = j * 2
-    return(d)
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    else:
+        return max(a_dictionary, key = a_dictionary.get)
