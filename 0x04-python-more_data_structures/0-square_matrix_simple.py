bead96b6d7700b1bd117042dfd9ca14bf4679c10
alaelbenna <elbenna.ala@outlook.com>
2022-02-05 04:32:27 -0800

    Matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    N_matrix = list(map(lambda a: list(map(lambda b : b ** 2, a)), matrix))
-    return N_matrix
+    return list(map(lambda x: list(map(lambda y: y ** 2, x)), matrix))
