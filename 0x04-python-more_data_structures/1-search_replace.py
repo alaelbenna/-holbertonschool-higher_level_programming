29d47c5c112fee60e5a7701d23a78c6bc6aca276
alaelbenna <elbenna.ala@outlook.com>
2022-02-05 04:38:27 -0800

    search replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new = []
+    N = []
     for i in my_list:
-        if (i == search):
-            new.append(replace)
+        if i == search:
+            N.append(replace)
         else:
-            new.append(i)
-        return (new)    
+            N.append(i)
+    return (N)    
