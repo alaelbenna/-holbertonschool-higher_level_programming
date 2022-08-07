b69a52bed9fe02b6dca5d0fc13f4931a7c9ae8f8
alaelbenna <https://github.com/alaelbenna>
2022-08-07 14:33:40 -0700

    Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,23 +1,26 @@
 #!/usr/bin/python3
-"""a script that lists all states from the database hbtn_0e_0_usa"""
+"""
+script that lists all states from the database hbtn_0e_0_usa
+"""
+
+
 import MySQLdb
 from sys import argv
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=argv[1]
-            passwd=argv[2]
-            database=argv[3]
-    )
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        password=argv[2],
+        database=argv[3],
 
+    )
     cursor = db.cursor()
-    sql = "SELECT * FROM states ORDER BY id")
+    sql = "SELECT * FROM states ORDER BY id"
     cursor.execute(sql)
-    results =cursor.fetchall()
+    results = cursor.fetchall()
     for row in results:
         print(row)
     cursor.close()
